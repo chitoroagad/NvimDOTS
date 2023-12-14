46876471e1bab6318fa61dea524f49aa599ac704
DariusC <darius.chitoroaga+gh@proton.me>
2023-12-14 19:38:06 +0000

    fixed nvim-tree warning

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -19,7 +19,7 @@ return {
 	"nvim-tree/nvim-tree.lua",
 	lazy = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = {
+	opts = {
 		sort_by = "case_sensitive",
 		view = {
 			width = 25,
