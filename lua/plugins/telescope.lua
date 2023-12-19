82d328986685d13568afa3015a2d99feb84ad979
DariusC <darius.chitoroaga+gh@proton.me>
2023-12-19 20:30:23 +0000

    changed telescope mapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ local keymap = vim.keymap
 
 local config = function()
 	local telescope = require("telescope")
+    local actions = require("telescope.actions")
 	telescope.setup({
 		pickers = {
 			find_files = {
@@ -11,7 +12,17 @@ local config = function()
 				theme = "dropdown",
 			},
 		},
+        defaults = {
+            mappings = {
+                i = {
+                    ["<C-j>"] = actions.move_selection_next,
+                    ["<C-k>"] = actions.move_selection_previous,
+                },
+            },
+		},
 	})
+
+    telescope.load_extension("fzf")
 end
 
 return {
@@ -29,3 +40,4 @@ return {
 		keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
 	},
 }
+
