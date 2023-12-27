41d2d5b69e32e8ba6b149528a4c26afb951c6235
DariusC <darius.chitoroaga+gh@proton.me>
2023-12-27 22:52:33 +0000

    removed unecessary code:

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,8 +4,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	group = lsp_fmt_group,
 	callback = function()
 		local null_ls = vim.lsp.get_active_clients({ name = "null-ls" })
-		local rust_analyzer = vim.lsp.get_active_clients({ name = "rust_analyzer" })
-		if vim.tbl_isempty(null_ls) and vim.tbl_isempty(rust_analyzer) then
+		if vim.tbl_isempty(null_ls) then
 			return
 		end
 
