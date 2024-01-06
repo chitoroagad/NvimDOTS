7fef52645e756aeb28a2d300642b2998d17a1897
DariusC <darius.chitoroaga+gh@proton.me>
2024-01-06 17:14:08 +0000

    update + fixed hardtime + added copilot

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,7 +2,7 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {
-		disabled_filetypes = { "fugitive" },
+		disabled_filetypes = { "fugitive", "NvimTree" },
 	},
 	event = { "BufEnter" },
 }
