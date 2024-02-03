47be1efd1b07db8cae28259802e822b2c03baaa8
DariusC <darius.chitoroaga+gh@proton.me>
2024-02-03 17:53:56 +0000

    changed flake8 max line len, added some files to hardtime-exclude

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,7 +2,17 @@ return {
 	"m4xshen/hardtime.nvim",
 	dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
 	opts = {
-		disabled_filetypes = { "lazy", "fugitive", "NvimTree", "noice", "lspinfo", "help" },
+		disabled_filetypes = {
+			"mason",
+			"undotree",
+			"null-ls-info",
+			"lazy",
+			"fugitive",
+			"NvimTree",
+			"noice",
+			"lspinfo",
+			"help",
+		},
 	},
 	event = { "BufEnter" },
 }
--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -15,7 +15,17 @@ return {
 				null_ls.builtins.formatting.rustfmt,
 
 				null_ls.builtins.diagnostics.luacheck,
-				null_ls.builtins.diagnostics.flake8,
+				null_ls.builtins.diagnostics.flake8.with({
+					args = {
+						"--format",
+						"default",
+						"--stdin-display-name",
+						"$FILENAME",
+						"-",
+						"--max-line-length",
+						"120",
+					},
+				}),
 				null_ls.builtins.diagnostics.eslint_d,
 				null_ls.builtins.diagnostics.shellcheck,
 				null_ls.builtins.diagnostics.hadolint,
